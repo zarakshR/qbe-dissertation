a14cae0d69a83c0dd18e9d29fbbce938bfc2315f
Zaraksh Rahman <zarakshR@pm.me>
2026-02-05 23:35:43 +0000

    stale code

--- util.c
+++ util.c
@@ -154,11 +154,6 @@ vgrow(void *vp, ulong len)
 	*(Vec **)vp = v1;
 }
 
-void* vadd(void* vp) {
-	Vec* v = *((Vec**) vp - 1);
-	assert(v + 1 && v->mag == VMag);
-}
-
 void
 addins(Ins **pvins, uint *pnins, Ins *i)
 {
